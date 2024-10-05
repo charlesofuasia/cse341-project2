98c532b6ead46414bb2dd133e82e51f19328eb03
Charles Ofuasia <ofu23001@byui.edu>
2024-10-05 09:03:18 +0100

    Added A welcome message to home

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ const swaggerDocument = require('../swagger.json');
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
+router.get('/', (req, res) => {
+  res.send('Personal Project API');
+});
 router.use('/patients', require('./patientsRoute'));
 
 router.use('/employees', require('./employeesRoute'));
