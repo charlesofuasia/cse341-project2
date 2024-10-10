2e4a261d227aac95db42f00a839123107cdff0a5
Charles Ofuasia <ofu23001@byui.edu>
2024-10-10 14:13:47 +0100

    debugging

--- swagger.js
+++ swagger.js
@@ -8,13 +8,13 @@ const doc = {
     description: 'Description',
   },
   host: 'cse341-project2-yijv.onrender.com',
-  schemes: ['http', 'htps'],
+  schemes: ['http', 'https'],
   securityDefinitions: {
     github_oauth: {
       type: 'oauth2',
       authorizationUrl: 'https://github.com/login/oauth/authorize',
       flow: 'implicit',
-      client_id: Process.env.GITHUB_CLIENT_ID,
+      client_id: process.env.GITHUB_CLIENT_ID,
       scopes: {
         'read:user': 'Read user information',
         repo: 'Access repositories',
